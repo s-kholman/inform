dd372391d182c3aa4c300c596506e8adaef0b28f
Sergey <uporovo@mail.ru>
2023-07-31 11:34:04 +0500

    Форма востановления пароля + перевод на русский + форма письма

---
+++ resources/views/auth/passwords/send.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('content')
+    <div>
+        <p>Мы отправили вам по электронной почте ссылку для сброса пароля.</p>
+    </div>
+    <a class="btn btn-info" href="/">На главную страницу</a>
+@endsection
