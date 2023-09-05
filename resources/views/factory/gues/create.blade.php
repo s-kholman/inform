51656c05130ba09cf940f6637fe8fe7b8bd25108
Sergey <uporovo@mail.ru>
2023-09-05 10:50:51 +0500

    Завод по переработке. Отчет - Поступающее на завод сырье

--- resources/views/factory/gues/create.blade.php
+++ resources/views/factory/gues/create.blade.php
@@ -9,7 +9,7 @@
 
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="volume">Объем</label>
                     <input placeholder="Введите поступивший объем"
                            class="form-control @error('volume') is-invalid @enderror"
@@ -33,9 +33,9 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="mechanical">Механические повреждение</label>
-                    <input placeholder="Введите %"
+                    <input placeholder="Введите %, в допуске {{$specifically['mechanical']}}%"
                            class="form-control @error('mechanical') is-invalid @enderror"
                            @if(empty(old('mechanical'))) value="{{$factory[0]->gues->mechanical ?? ''}}" @else value="{{old('mechanical')}}" @endif
                            id="mechanical"
@@ -51,9 +51,9 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="land">Камни, земля</label>
-                    <input placeholder="Введите %"
+                    <input placeholder="Введите %, в допуске {{$specifically['land']}}%"
                            class="form-control @error('land') is-invalid @enderror"
                            @if(empty(old('land'))) value="{{$factory[0]->gues->land ?? ''}}" @else value="{{old('land')}}" @endif
                            id="land"
@@ -69,9 +69,9 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="haulm">Ботва</label>
-                    <input placeholder="Введите %"
+                    <input placeholder="Введите %, в допуске {{$specifically['haulm']}}%"
                            class="form-control @error('haulm') is-invalid @enderror"
                            @if(empty(old('haulm'))) value="{{$factory[0]->gues->haulm ?? ''}}" @else value="{{old('haulm')}}" @endif
                            id="haulm"
@@ -87,9 +87,9 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="rot">Гниль</label>
-                    <input placeholder="Введите %"
+                    <input placeholder="Введите %, в допуске {{$specifically['rot']}}%"
                            class="form-control @error('rot') is-invalid @enderror"
                            @if(empty(old('rot'))) value="{{$factory[0]->gues->rot ?? ''}}" @else value="{{old('rot')}}" @endif
                            id="rot"
@@ -105,25 +105,25 @@
             </div>
 
             <div class="row">
-                <div class="col-2 form-check form-switch">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 form-check form-switch">
                     <label for="foreign_object">Инородные предметы</label>
                     <input class="form-check-input" type="checkbox" @if($factory[0]->gues->foreign_object ?? false) checked @endif id="foreign_object" name="foreign_object">
                 </div>
-                <div class="col-2 form-check form-switch">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 form-check form-switch">
                     <label for="another_variety">Пересортица</label>
                     <input class="form-check-input" type="checkbox" @if($factory[0]->gues->another_variety ?? false) checked @endif  id="another_variety" name="another_variety">
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4p-2">
                     <hr>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
-                    <label for="less_thirty">до 35</label>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
+                    <label for="less_thirty">Фракция до 35</label>
                     <input placeholder="Введите %"
                            class="form-control @error('less_thirty') is-invalid @enderror"
                            @if(empty(old('less_thirty'))) value="{{$factory[0]->gues->less_thirty ?? ''}}" @else value="{{old('less_thirty')}}" @endif
@@ -140,8 +140,8 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
-                    <label for="thirty">35-45</label>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
+                    <label for="thirty">Фракция 35-45</label>
                     <input placeholder="Введите %"
                            class="form-control @error('thirty') is-invalid @enderror"
                            @if(empty(old('thirty'))) value="{{$factory[0]->gues->thirty ?? ''}}" @else value="{{old('thirty')}}" @endif
@@ -158,8 +158,8 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
-                    <label for="forty">45-55</label>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
+                    <label for="forty">Фракция 45-55</label>
                     <input placeholder="Введите %"
                            class="form-control @error('forty') is-invalid @enderror"
                            @if(empty(old('forty'))) value="{{$factory[0]->gues->forty ?? ''}}" @else value="{{old('forty')}}" @endif
@@ -176,8 +176,8 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
-                    <label for="fifty">55-65</label>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
+                    <label for="fifty">Фракция 55-65</label>
                     <input placeholder="Введите %"
                            class="form-control @error('fifty') is-invalid @enderror"
                            @if(empty(old('fifty'))) value="{{$factory[0]->gues->fifty ?? ''}}" @else value="{{old('fifty')}}" @endif
@@ -194,8 +194,8 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
-                    <label for="sixty">65</label>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
+                    <label for="sixty">Фракция 65</label>
                     <input placeholder="Введите %"
                            class="form-control @error('sixty') is-invalid @enderror"
                            @if(empty(old('sixty'))) value="{{$factory[0]->gues->sixty ?? ''}}" @else value="{{old('sixty')}}" @endif
@@ -212,7 +212,7 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <hr>
                 </div>
             </div>
@@ -220,7 +220,7 @@
             <input hidden name="factory_material_id" value="{{$factory[0]->id}}">
 
         <div class="row">
-            <div class="col-4 p-2">
+            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                 <input hidden class="display form-control @error('full') is-invalid @enderror" name="full">
                 @error('full')
                 <span class="invalid-feedback">
@@ -231,16 +231,16 @@
         </div>
 
             <div class="row">
-                <div class="col-2 p-2 ">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-2 ">
                     <a class="btn btn-primary" href="/factory/material">Назад</a>
                 </div>
-                <div class="col-2 p-2 text-end">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-2 text-end">
                     <input class="btn btn-primary" type="submit" value="Сохранить" name="save">
                 </div>
             </div>
     </form>
         <div class="row">
-            <div class="col-4 text-center">
+            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 text-center">
                 <form action="{{route('material.destroy', ['material' => $factory[0]->id])}}" method="post">
                     @csrf
                     @method('DELETE')
--- resources/views/factory/material/create.blade.php
+++ resources/views/factory/material/create.blade.php
@@ -7,7 +7,7 @@
         <div class="container gx-4">
             <div class="row">
                     <label class="float-end" for="date">Дата</label>
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <input type="date"
                            class="display form-control @error('date') is-invalid @enderror"
                            id="date"
@@ -23,7 +23,7 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="filial_name">Филиал поступления</label>
                     <select class="form-select @error('filial_name') is-invalid @enderror" id="filial_name"
                             name="filial_name">
@@ -44,7 +44,7 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="fio">Введите ФИО</label>
                     <input placeholder="Введите ФИО"
                            class="form-control @error('fio') is-invalid @enderror"
@@ -60,7 +60,7 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="nomenklature">Номенклатура</label>
                     <select class="form-select @error('nomenklature') is-invalid @enderror" id="nomenklature"
                             name="nomenklature">
@@ -81,7 +81,7 @@
             </div>
 
             <div class="row">
-                <div class="col-4 p-2">
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 p-2">
                     <label for="image">Выберите изображение</label>
                     <input class="form-control form-select @error('image') is-invalid @enderror"
                            type="file"
@@ -100,10 +100,10 @@
 
             </div>
             <div class="row">
-                <div class="col-2 p-2">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-2">
                     <input class="btn btn-primary" type="submit" value="Сохранить" name="save">
                 </div>
-                <div class="col-2 p-2 text-end">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-2 text-end">
                     <a class="btn btn-primary" href="/factory/material">Назад</a>
                 </div>
             </div>
