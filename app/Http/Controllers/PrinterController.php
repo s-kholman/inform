ed9db42e10294b28e6ffe551cbd1196c229035a2
Sergey <uporovo@mail.ru>
2023-09-25 10:00:31 +0500

    Исправление: 1. Убрана избыточность массива 2. Убран не нужный цикл и перенесено в метод 3. Имена свойст приведены к одному наименованию 4. Убран лишний запрос к устройству

--- app/Http/Controllers/PrinterController.php
+++ app/Http/Controllers/PrinterController.php
@@ -80,21 +80,23 @@ class PrinterController extends Controller
                     try {
                         $get_snmp = $snmp->getValue($oid);
                         if ($get_snmp == '') {
-                            if (array_key_exists($item->device_id, $out)) {
-                                unset($out[$item->device_id]);
-                            }
+                            unset($out);
                             break;
                         } else {
-                            $out [$item->device_id] [$oid] = $snmp->getValue($oid);
+                            $out [$oid] = $get_snmp;
                         }
 
                     } catch (ConnectionException) {
-                        if (array_key_exists($item->device_id, $out)) {
-                            unset($out[$item->device_id]);
+                            unset($out);
                             break;
-                        }
                     }
                 }
+            }
+            if (!empty($out))
+            {
+                $this->store($item->device_id, $out);
+                dump($item->ip);echo '<br>'; dump($out); echo '<br><br>';
+                unset($out);
             }
         }
         /**
@@ -104,6 +106,7 @@ class PrinterController extends Controller
          * Страницы получаем с function count
          */
 
+/*
         foreach ($out as $device => $value) {
             DailyUse::updateOrCreate(
                 [
@@ -115,16 +118,16 @@ class PrinterController extends Controller
                     'count' => $this->count($value, $device)
                 ]);
         }
-
+*/
         dump(Carbon::now()->format('i:s:u'));
         return 'Выполнение скрипта оконченно,';
 
     }
 
-    private function kyocera($data)
+    private function kyocera($value)
     {
-        if (array_key_exists('1.3.6.1.2.1.43.11.1.1.8.1.1', $data) && array_key_exists('1.3.6.1.2.1.43.11.1.1.9.1.1', $data)) {
-            $toner = $data['1.3.6.1.2.1.43.11.1.1.9.1.1'] / ($data['1.3.6.1.2.1.43.11.1.1.8.1.1'] / 100);
+        if (array_key_exists('1.3.6.1.2.1.43.11.1.1.8.1.1', $value) && array_key_exists('1.3.6.1.2.1.43.11.1.1.9.1.1', $value)) {
+            $toner = $value['1.3.6.1.2.1.43.11.1.1.9.1.1'] / ($value['1.3.6.1.2.1.43.11.1.1.8.1.1'] / 100);
         } else {
             $toner = 100;
         }
@@ -145,5 +148,18 @@ class PrinterController extends Controller
         } else {
             return DailyUse::where('device_id', $device)->latest('date')->take(1)->value('count');
         }
+    }
+
+    private function store(int $device,array $value):void
+    {
+        DailyUse::updateOrCreate(
+            [
+                'device_id' => $device,
+                'date' => Carbon::now()
+            ],
+            [
+                'toner' => $this->kyocera($value),
+                'count' => $this->count($value, $device)
+            ]);
     }
 }
