351200720f7943f02f9f323064d7792ffdee5739
Sergey <uporovo@mail.ru>
2024-10-31 19:33:40 +0500

    fix script vpn

--- app/Http/Controllers/Cabinet/SSL/CreateScriptController.php
+++ app/Http/Controllers/Cabinet/SSL/CreateScriptController.php
@@ -22,8 +22,8 @@ class CreateScriptController extends Controller
                 'Start-Process powershell.exe -Verb RunAs -ArgumentList (\'-noprofile -noexit -file "{0}" -elevated\' -f ( $myinvocation.MyCommand.Definition ))'.PHP_EOL.
                 'Break}'.PHP_EOL.
                 'Get-VpnConnection | Where-Object { $_.Name -eq \'VPN-KRiMM\'} | Remove-VpnConnection -Name "VPN-KRiMM" -Force'.PHP_EOL.
-                'Add-VpnConnection -Name "VPN-KRiMM" -ServerAddress "welcome.krimm.ru" -TunnelType Ikev2 -AuthenticationMethod MachineCertificate -SplitTunneling -PassThru'.PHP_EOL.
-                'Add-VpnConnectionRoute -ConnectionName "VPN-KRiMM" -DestinationPrefix %s/32 –PassThru'.PHP_EOL.
+                'Add-VpnConnection -Name "VPN-KRiMM" -ServerAddress "welcome.krimm.ru" -TunnelType Ikev2 -AuthenticationMethod MachineCertificate -SplitTunneling'.PHP_EOL.
+                'Add-VpnConnectionRoute -ConnectionName "VPN-KRiMM" -DestinationPrefix %s/32'.PHP_EOL.
                 '$DesktopPath = [Environment]::GetFolderPath("Desktop")'.PHP_EOL.
                 '$OFS = "`r`n"'.PHP_EOL.
                 '$msg = "username:s:krimm\%s" + $OFS + "full address:s:%s"'.PHP_EOL.
