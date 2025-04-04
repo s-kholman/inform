2aeb3f7a4ef1764559a71917ae5ccb8c37afd0fc
s-kholman <102976069+s-kholman@users.noreply.github.com>
2025-04-04 09:10:13 +0500

    Revert "fix - printer"

--- app/Actions/Printer/device/DeviceIndexAction.php
+++ app/Actions/Printer/device/DeviceIndexAction.php
@@ -8,13 +8,11 @@ class DeviceIndexAction
 {
     public function __invoke()
     {
-        return CurrentStatus::query()
-            ->with('status')
-            ->get()
-            ->sortByDesc('date')
-            ->unique('device_id')
-            ->sortBy('filial.name')
-        ;
+        $status = CurrentStatus::with('status')->get();
+        return $status->
+        sortByDesc('date')->
+        unique(['device_id'])->
+        sortBy('filial.name');
     }
 
 }
